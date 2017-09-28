8000e5c684f2196d05c653d70e8b195f1d454138
Deyvisson <deyvisson1993@gmail.com>
2017-09-28 17:08:43 -0300

    Deyvisson Mendes - Projeto LavaJato 28/09 as 17:00

---
+++ LavaJato - 28-09 as 17.06h/src/br/com/LavaJato/util/conexao.java
@@ -0,0 +1,24 @@
+package br.com.LavaJato.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+public class conexao {
+	
+public static Connection getConexeao() throws Exception{
+		
+		Connection conn = null;
+			
+	String url_conection = "jdbc:mysql://localhost:3306/lavaJatoProjeto";
+	String usuario = "root";
+	String senha ="889626";
+	
+	DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+		
+	return conn = DriverManager.getConnection(url_conection,usuario,senha);
+		
+	}
+}
